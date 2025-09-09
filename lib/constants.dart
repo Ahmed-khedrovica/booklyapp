c8ac9aaa7fef498dff50f26b4443865970f29bae
Your Name <you@example.com>
2025-09-10 01:42:58 +0300

    add custom app bar to home

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const backgroundColor = Color(0xff100B20);
 // const Duration transitionDuration = Duration(seconds: 2);
+const EdgeInsets padding = EdgeInsets.symmetric(horizontal: 24, vertical: 16);
