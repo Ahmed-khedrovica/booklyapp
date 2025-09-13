b87d6eaee59389c7b0e7b3d9c54e89079342af81
Your Name <you@example.com>
2025-09-14 00:19:43 +0300

    best seller item part three

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -54,6 +54,7 @@ class BestSellerListViewItem extends StatelessWidget {
           ),
           SizedBox(width: 30,),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 width: MediaQuery.of(context).size.width * .5,
@@ -64,6 +65,25 @@ class BestSellerListViewItem extends StatelessWidget {
                   overflow: TextOverflow.ellipsis,
                   ),
               ),
+              const SizedBox(
+                height: 3,
+              ),
+              Text(
+                  'J.K Howling',
+                style: Styles.textStyle20.copyWith(
+                  color: Colors.grey
+                ),
+              ),
+              const SizedBox(
+                height: 3,
+              ),
+              Row(
+                children: [
+                Text(
+                  '19.99',
+                style: Styles.textStyle18,
+                ),
+              ],)
             ],
           ),
         ],
