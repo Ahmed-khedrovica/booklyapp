c91402fb0a8e9be2f4d9c8c394419011c940bfe9
Your Name <you@example.com>
2025-09-13 04:48:42 +0300

    best seller list view item | part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -20,7 +20,39 @@ class HomeViewBody extends StatelessWidget {
       Text(
         'Best Seller',
       style: Styles.titleMedium,
-      )
+      ),
+          BestSellerListViewItem(),
     ]);
+  }
+}
+
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 120,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: NetworkImage(
+                    'https://www.surfer.com/.image/t_share/MTk2Mjc2ODA4ODk3ODY1MjQz/oct_cover_guesser.jpg',
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
